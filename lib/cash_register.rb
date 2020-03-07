7c73e84b347f5bb49fc351a79373da66c5ef6a4d
Jenise Ruehle <jela07@gmail.com>
2020-03-07 20:55:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :item
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+end 
